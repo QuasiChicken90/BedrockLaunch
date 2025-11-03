907b8e703074dc4492ae6c67b3d3a85d759a434c
QuasiChicken90 <jaeden.naidoo@icloud.com>
2025-11-03 21:41:20 +0200

    Update launcher_restart.bat

--- launcher_restart.bat
+++ launcher_restart.bat
@@ -1,11 +1,16 @@
 @echo off
-timeout /t 5 /nobreak >nul
+setlocal
 
-powershell -Command "Expand-Archive -Force 'update.zip' '.'"
+taskkill /IM neu_wv.exe /F /T >nul 2>&1
+taskkill /IM launcher.exe /F /T >nul
+
+timeout /t 2 /nobreak >nul
+
+tar -xf update.zip
 
 del /f /q "update.zip"
 del /f /q "%~dp0\App\welcome.txt"
 
-start "" "Launcher.exe"
+start "Launcher.exe"
 
 exit
