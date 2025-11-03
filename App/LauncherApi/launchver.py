d8e19680555a8adfd8d65af780e58320e2d6b463
QuasiChicken90 <jaeden.naidoo@icloud.com>
2025-11-03 21:42:47 +0200

    Update launchver.py

--- App/LauncherApi/launchver.py
+++ App/LauncherApi/launchver.py
@@ -13,6 +13,6 @@ def launch(version):
         os.system(f'powershell.exe -Command "Get-AppxPackage -allusers *minecraftUWP* | Remove-AppxPackage -allusers"')
         os.system(f"powershell.exe Add-AppxPackage -Register Library/Installations/{version}/AppXManifest.xml")
         webbrowser.open("minecraft://")
-        os.kill(os.getpid(), signal.SIGTERM)
+        # os.kill(os.getpid(), signal.SIGTERM) no no no no no no no no
 
 
