d55c8faff2bd9d3f01142bceef37303eb089aed2
QuasiChicken90 <jaeden.naidoo@icloud.com>
2026-09-04 15:50:09 +0200

    Update launcher_restart.bat

--- launcher_restart.bat
+++ launcher_restart.bat
@@ -1,11 +1,12 @@
 @echo off
 title BedrockLaunch Updating...
 
-taskkill /F /IM Launcher.exe 2>nul
-taskkill /F /IM neu_wv.exe 2>nul
+taskkill /F /IM BedrockLaunch.exe 2>nul
+
+taskkill /F /IM Backend.exe 2>nul
 
 timeout /t 2 /nobreak >nul
 
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Expand-Archive -Path 'update.zip' -DestinationPath '.' -Force; if (Test-Path 'App\welcome.txt') { Remove-Item 'App\welcome.txt' -Force }; Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Update complete! Please open Launcher.exe to continue.', 'Update Installed', 'OK', 'Information')"
+powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Expand-Archive -Path 'update.zip' -DestinationPath '.' -Force; Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Update complete! Please open BedrockLaunch.exe to continue.', 'Update Installed', 'OK', 'Information')"
 
 exit
